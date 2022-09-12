67ea2807c9d7642194f67fc2709a2d1e51e13300
Hazal Sendogan <hazal.sendogan@maarifa.com.tr>
2022-09-12 13:14:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PeriodicTable from './PeriodicTable';
+
+function App() {
+  return (
+    <div>
+        <PeriodicTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PeriodicTable.js
@@ -0,0 +1,497 @@
+import React, { Component } from "react";
+
+export default class PeriodicTable extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Periodic Table with CSS Grid</h1>
+        <div class="grid-container-1">
+          <div class="row-1 column1 cyan">
+            <div class="top">1</div>
+            <div class="bottom">H</div>
+          </div>
+          <div class="row-1 column18 dodgerblue">
+            <div class="top">2</div>
+            <div class="bottom">He</div>
+          </div>
+          <div class="row-2 column1 gold">
+            <div class="top">3</div>
+            <div class="bottom">Li</div>
+          </div>
+          <div class="row-2 column2 khaki">
+            <div class="top">4</div>
+            <div class="bottom">Be</div>
+          </div>
+          <div class="row-2 column13 pink">
+            <div class="top">5</div>
+            <div class="bottom">B</div>
+          </div>
+          <div class="row-2 column14 cyan">
+            <div class="top">6</div>
+            <div class="bottom">C</div>
+          </div>
+          <div class="row-2 column15 cyan">
+            <div class="top">7</div>
+            <div class="bottom">N</div>
+          </div>
+          <div class="row-2 column16 cyan">
+            <div class="top">8</div>
+            <div class="bottom">O</div>
+          </div>
+          <div class="row-2 column17 green">
+            <div class="top">9</div>
+            <div class="bottom">F</div>
+          </div>
+          <div class="row-2 column18 dodgerblue">
+            <div class="top">10</div>
+            <div class="bottom">Ne</div>
+          </div>
+          <div class="row-3 column1 gold">
+            <div class="top">11</div>
+            <div class="bottom">Na</div>
+          </div>
+          <div class="row-3 column2 khaki">
+            <div class="top">12</div>
+            <div class="bottom">Mg</div>
+          </div>
+          <div class="row-3 column13 orange">
+            <div class="top">13</div>
+            <div class="bottom">Al</div>
+          </div>
+          <div class="row-3 column14 pink">
+            <div class="top">14</div>
+            <div class="bottom">Si</div>
+          </div>
+          <div class="row-3 column15 cyan">
+            <div class="top">15</div>
+            <div class="bottom">P</div>
+          </div>
+          <div class="row-3 column16 cyan">
+            <div class="top">16</div>
+            <div class="bottom">S</div>
+          </div>
+          <div class="row-3 column17 green">
+            <div class="top">17</div>
+            <div class="bottom">Cl</div>
+          </div>
+          <div class="row3 column18 dodgerblue">
+            <div class="top">18</div>
+            <div class="bottom">Ar</div>
+          </div>
+          <div class="row-4 gold">
+            <div class="top">19</div>
+            <div class="bottom">K</div>
+          </div>
+          <div class="row-4 khaki">
+            <div class="top">20</div>
+            <div class="bottom">Ca</div>
+          </div>
+          <div class="row-4 coral">
+            <div class="top">21</div>
+            <div class="bottom">Sc</div>
+          </div>
+          <div class="row-4 coral">
+            <div class="top">22</div>
+            <div class="bottom">Ti</div>
+          </div>
+          <div class="row-4 coral">
+            <div class="top">23</div>
+            <div class="bottom">V</div>
+          </div>
+          <div class="row-4 coral">
+            <div class="top">24</div>
+            <div class="bottom">Cr</div>
+          </div>
+          <div class="row-4 coral">
+            <div class="top">25</div>
+            <div class="bottom">Mn</div>
+          </div>
+          <div class="row-4 coral">
+            <div class="top">26</div>
+            <div class="bottom">Fe</div>
+          </div>
+          <div class="row-4 coral">
+            <div class="top">27</div>
+            <div class="bottom">Co</div>
+          </div>
+          <div class="row-4 coral">
+            <div class="top">28</div>
+            <div class="bottom">Ni</div>
+          </div>
+          <div class="row-4 coral">
+            <div class="top">29</div>
+            <div class="bottom">Cu</div>
+          </div>
+          <div class="row-4 coral">
+            <div class="top">30</div>
+            <div class="bottom">Zn</div>
+          </div>
+          <div class="row-4 orange">
+            <div class="top">31</div>
+            <div class="bottom">Ga</div>
+          </div>
+          <div class="row-4 pink">
+            <div class="top">32</div>
+            <div class="bottom">Ge</div>
+          </div>
+          <div class="row-4 pink">
+            <div class="top">33</div>
+            <div class="bottom">As</div>
+          </div>
+          <div class="row-4 cyan">
+            <div class="top">34</div>
+            <div class="bottom">Se</div>
+          </div>
+          <div class="row-4 green">
+            <div class="top">35</div>
+            <div class="bottom">Br</div>
+          </div>
+          <div class="row-4 dodgerblue">
+            <div class="top">36</div>
+            <div class="bottom">Kr</div>
+          </div>
+          <div class="row-5 gold">
+            <div class="top">37</div>
+            <div class="bottom">Rb</div>
+          </div>
+          <div class="row-5 khaki">
+            <div class="top">38</div>
+            <div class="bottom">Sr</div>
+          </div>
+          <div class="row-5 coral">
+            <div class="top">39</div>
+            <div class="bottom">Y</div>
+          </div>
+          <div class="row-5 coral">
+            <div class="top">40</div>
+            <div class="bottom">Zr</div>
+          </div>
+          <div class="row-5 coral">
+            <div class="top">41</div>
+            <div class="bottom">Nb</div>
+          </div>
+          <div class="row-5 coral">
+            <div class="top">42</div>
+            <div class="bottom">Mo</div>
+          </div>
+          <div class="row-5 coral">
+            <div class="top">43</div>
+            <div class="bottom">Tc</div>
+          </div>
+          <div class="row-5 coral">
+            <div class="top">44</div>
+            <div class="bottom">Ru</div>
+          </div>
+          <div class="row-5 coral">
+            <div class="top">45</div>
+            <div class="bottom">Rh</div>
+          </div>
+          <div class="row-5 coral">
+            <div class="top">46</div>
+            <div class="bottom">Pd</div>
+          </div>
+          <div class="row-5 coral">
+            <div class="top">47</div>
+            <div class="bottom">Ag</div>
+          </div>
+          <div class="row-5 coral">
+            <div class="top">48</div>
+            <div class="bottom">Cd</div>
+          </div>
+          <div class="row-5 orange">
+            <div class="top">49</div>
+            <div class="bottom">In</div>
+          </div>
+          <div class="row-5 orange">
+            <div class="top">50</div>
+            <div class="bottom">Sn</div>
+          </div>
+          <div class="row-5 pink">
+            <div class="top">51</div>
+            <div class="bottom">Sb</div>
+          </div>
+          <div class="row-5 pink">
+            <div class="top">52</div>
+            <div class="bottom">Te</div>
+          </div>
+          <div class="row-5 green">
+            <div class="top">53</div>
+            <div class="bottom">I</div>
+          </div>
+          <div class="row-5 dodgerblue">
+            <div class="top">54</div>
+            <div class="bottom">Xe</div>
+          </div>
+          <div class="row-6 gold">
+            <div class="top">55</div>
+            <div class="bottom">Cs</div>
+          </div>
+          <div class="row-6 khaki">
+            <div class="top">56</div>
+            <div class="bottom">Ba</div>
+          </div>
+          <div class="row-6 rosy">
+            <div class="top">57-71</div>
+            <div class="bottom">La</div>
+          </div>
+          <div class="row-6 coral">
+            <div class="top">72</div>
+            <div class="bottom">Hf</div>
+          </div>
+          <div class="row-6 coral">
+            <div class="top">73</div>
+            <div class="bottom">Ta</div>
+          </div>
+          <div class="row-6 coral">
+            <div class="top">74</div>
+            <div class="bottom">W</div>
+          </div>
+          <div class="row-6 coral">
+            <div class="top">75</div>
+            <div class="bottom">Re</div>
+          </div>
+          <div class="row-6 coral">
+            <div class="top">76</div>
+            <div class="bottom">Os</div>
+          </div>
+          <div class="row-6 coral">
+            <div class="top">77</div>
+            <div class="bottom">Ir</div>
+          </div>
+          <div class="row-6 coral">
+            <div class="top">78</div>
+            <div class="bottom">Pt</div>
+          </div>
+          <div class="row-6 coral">
+            <div class="top">79</div>
+            <div class="bottom">Au</div>
+          </div>
+          <div class="row-6 coral">
+            <div class="top">80</div>
+            <div class="bottom">Hg</div>
+          </div>
+          <div class="row-6 orange">
+            <div class="top">81</div>
+            <div class="bottom">Tl</div>
+          </div>
+          <div class="row-6 orange">
+            <div class="top">82</div>
+            <div class="bottom">Pb</div>
+          </div>
+          <div class="row-6 orange">
+            <div class="top">83</div>
+            <div class="bottom">Bi</div>
+          </div>
+          <div class="row-6 pink">
+            <div class="top">84</div>
+            <div class="bottom">Po</div>
+          </div>
+          <div class="row-6 green">
+            <div class="top">85</div>
+            <div class="bottom">At</div>
+          </div>
+          <div class="row-6 dodgerblue">
+            <div class="top">86</div>
+            <div class="bottom">Pn</div>
+          </div>
+          <div class="row-7 gold">
+            <div class="top">87</div>
+            <div class="bottom">Fa</div>
+          </div>
+          <div class="row-7 khaki">
+            <div class="top">88</div>
+            <div class="bottom">Ra</div>
+          </div>
+          <div class="row-7 seagreen">
+            <div class="top">89-103</div>
+            <div class="bottom">Ac</div>
+          </div>
+          <div class="row-7 coral">
+            <div class="top">104</div>
+            <div class="bottom">Rf</div>
+          </div>
+          <div class="row-7 coral">
+            <div class="top">105</div>
+            <div class="bottom">Db</div>
+          </div>
+          <div class="row-7 coral">
+            <div class="top">106</div>
+            <div class="bottom">Sg</div>
+          </div>
+          <div class="row-7 coral">
+            <div class="top">107</div>
+            <div class="bottom">Bh</div>
+          </div>
+          <div class="row-7 coral">
+            <div class="top">108</div>
+            <div class="bottom">Hs</div>
+          </div>
+          <div class="row-7 coral">
+            <div class="top">109</div>
+            <div class="bottom">Mt</div>
+          </div>
+          <div class="row-7 coral">
+            <div class="top">110</div>
+            <div class="bottom">Ds</div>
+          </div>
+          <div class="row-7 coral">
+            <div class="top">111</div>
+            <div class="bottom">Rg</div>
+          </div>
+          <div class="row-7 coral">
+            <div class="top">112</div>
+            <div class="bottom">Cn</div>
+          </div>
+          <div class="row-7 orange">
+            <div class="top">113</div>
+            <div class="bottom">Uut</div>
+          </div>
+          <div class="row-7 orange">
+            <div class="top">114</div>
+            <div class="bottom">Fl</div>
+          </div>
+          <div class="row-7 orange">
+            <div class="top">115</div>
+            <div class="bottom">Uup</div>
+          </div>
+          <div class="row-7 orange">
+            <div class="top">116</div>
+            <div class="bottom">Lv</div>
+          </div>
+          <div class="row-7 green">
+            <div class="top">117</div>
+            <div class="bottom">Uus</div>
+          </div>
+          <div class="row-7 dodgerblue">
+            <div class="top">118</div>
+            <div class="bottom">Uuo</div>
+          </div>
+        </div>
+        <div class="grid-container-2">
+          <div class="left-1">Lantinit</div>
+          <div class="rosy">
+            <div class="top">57</div>
+            <div class="bottom">La</div>
+          </div>
+          <div class="rosy">
+            <div class="top">58</div>
+            <div class="bottom">Ce</div>
+          </div>
+          <div class="rosy">
+            <div class="top">59</div>
+            <div class="bottom">Pr</div>
+          </div>
+          <div class="rosy">
+            <div class="top">60</div>
+            <div class="bottom">Nd</div>
+          </div>
+          <div class="rosy">
+            <div class="top">61</div>
+            <div class="bottom">Pm</div>
+          </div>
+          <div class="rosy">
+            <div class="top">62</div>
+            <div class="bottom">Sm</div>
+          </div>
+          <div class="rosy">
+            <div class="top">63</div>
+            <div class="bottom">Eu</div>
+          </div>
+          <div class="rosy">
+            <div class="top">64</div>
+            <div class="bottom">Gd</div>
+          </div>
+          <div class="rosy">
+            <div class="top">65</div>
+            <div class="bottom">Tb</div>
+          </div>
+          <div class="rosy">
+            <div class="top">66</div>
+            <div class="bottom">Dy</div>
+          </div>
+          <div class="rosy">
+            <div class="top">67</div>
+            <div class="bottom">Ho</div>
+          </div>
+          <div class="rosy">
+            <div class="top">68</div>
+            <div class="bottom">Er</div>
+          </div>
+          <div class="rosy">
+            <div class="top">69</div>
+            <div class="bottom">Tm</div>
+          </div>
+          <div class="rosy">
+            <div class="top">70</div>
+            <div class="bottom">Yb</div>
+          </div>
+          <div class="rosy">
+            <div class="top">71</div>
+            <div class="bottom">Lu</div>
+          </div>
+          <div class="left-2">Aktinit</div>
+          <div class="seagreen">
+            <div class="top">89</div>
+            <div class="bottom">Ac</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">90</div>
+            <div class="bottom">Th</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">91</div>
+            <div class="bottom">Pa</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">92</div>
+            <div class="bottom">U</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">93</div>
+            <div class="bottom">Np</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">94</div>
+            <div class="bottom">Pu</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">95</div>
+            <div class="bottom">Am</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">96</div>
+            <div class="bottom">Cm</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">97</div>
+            <div class="bottom">Bk</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">98</div>
+            <div class="bottom">Cf</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">99</div>
+            <div class="bottom">Es</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">100</div>
+            <div class="bottom">Fm</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">101</div>
+            <div class="bottom">Md</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">102</div>
+            <div class="bottom">No</div>
+          </div>
+          <div class="seagreen">
+            <div class="top">103</div>
+            <div class="bottom">Lr</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
